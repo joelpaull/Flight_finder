ec73f723643fd0d56bf5de16f350d8162b3bb725
Joel Paull <joelpaull@joels-air.home>
2023-05-16 08:51:35 +0100

    function to register users started

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 import requests
 import sqlite3
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
 import os.path
-from helpers.user_manager import UserManager
+from helpers.user_manager import UserManager, login_required
 
 
 # Configure Flask 
 app = Flask(__name__)
+app.secret_key = 'jp1996'
 
 # Configure database
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -15,26 +16,73 @@ DB_PATH = os.path.join(BASE_DIR, "flights.sqlite")
 
 # Create user_manager object
 user_manager = UserManager()
-# user_manager.add_user("joel","joel", DB_PATH)
-current_user = ""
-logged_in = False
 
 
 @app.route("/")
 def index():
     return redirect("/login")
 
-@app.route("/login")
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    ''' Allow user to Login'''
-    if current_user == "":
+
+    # Clear any current user sessions
+    session.clear()
+
+    # Allow user to login
+    if request.method == "GET":
+        ''' Allow user to Login'''
         return render_template("login.html")
     
-    # Else show homepage
+    else:
+        # Retrieve details from posted form if POST
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Check database for user details
+        valid_user = user_manager.check_user(username, password, DB_PATH)
+
+        # if username in database and password matches, log user in
+        if valid_user:
+            session["username"] = username
+            return render_template("/homepage")
+        else:
+            # TODO
+            return render_template("error.html")
+
     
-@app.route("/register")
+
+@app.route("/register", methods = ["GET", "POST"])
 def register():
-    pass
+    if request.method == "GET":
+        ''' Allow user to Register'''
+        return render_template("register.html")
+    
+    # Retrieve details from posted form if POST
+    else: 
+        username = request.form.get("username")
+        password = request.form.get("password")
+        pass_confirmation = request.form.get("confirmation")
+
+        # Check passwords match and add to database 
+        if password == pass_confirmation:
+            user_added = user_manager.add_user(username, password, DB_PATH)
+        else:
+            error = "Sorry, Passwords Do Not Match."
+            render_template("error.html", error = error)
+
+        # Show result of user addition
+        if user_added:
+            render_template("register_success.html", username = username)
+        else:
+            # TODO
+            error = "Username Taken."
+            return render_template("error.html", error = error)
+        
+
+@app.route("/homepage")
+def homepage():
+    return render_template("homepage.html")
 
 
 
--- helpers/user_manager.py
+++ helpers/user_manager.py
@@ -1,11 +1,37 @@
 import sqlite3
+from flask import redirect, render_template, request, session
+from functools import wraps
 
 class UserManager():
 
     def __call__(self):
         pass
     
-    def add_user(self, username, password, db_path):
+
+    def check_user(self, username, password, db_path):
+        '''Query DB for username and check password matches'''
         with sqlite3.connect(db_path) as db:
-            db.execute("INSERT INTO user (username, password) VALUES (?, ?)", (username, password))
-            
+            row = db.execute("SELECT * FROM user WHERE username = ?", (username,)).fetchall()
+            if len(row) != 1 or password != row[0][1]:
+                return False
+            return True
+    
+
+    def add_user(self, username, password, db_path):
+        ''' Add user'''
+        with sqlite3.connect(db_path) as db:
+            try:
+                db.execute("INSERT INTO user (username, password) VALUES (?, ?)", (username, password))
+            except sqlite3.IntegrityError:
+                return False
+        return True
+
+
+def login_required(f):
+# Decorate routes to require login. https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/ 
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
